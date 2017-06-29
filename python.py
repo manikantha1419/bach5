7271e2604b9bf8459c62385b5d991b94aecb503c
manikantha1419 <manikanthadesu@gmail.com>
2017-06-29 18:00:27 +0530

    add content

---
+++ python.py
@@ -0,0 +1,13 @@
+i=1
+while	i<=3:
+
+	uname=raw_input("Enter a username : ")
+	pwd=raw_input("Enter a password : ")
+
+if	uname=="devendar" and pwd=="reddy":
+	print "Login Success"
+	break
+else :	
+	if 1==3:
+	print "Invalid username or password"
+	break
